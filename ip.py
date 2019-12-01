021d8930c61b86b942cd4efa61bf5193ff115cf2
administrator <administrator@bj11123.lan>
2019-12-01 15:50:19 +0800

    first add

---
+++ ip.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+def ip2uint(ip):
+    numbs = ip.split('.')
+    if len(numbs) != 4:
+        return -1
+    numbs = [int(i) for i in numbs if int(i) >= 0 or int(i) < 256]
+    if len(numbs) != 4:
+        return -1
+    numb = 0xFF & numbs[0]
+    numb = (numb << 8)+numbs[1]
+    numb = (numb << 8)+numbs[2]
+    numb = (numb << 8)+numbs[3]
+    return numb
+
+
+def iuint2ip(numb):
+    ip = list()
+    ip.append(str(numb & 0xff))
+    ip.append(str((numb >> 8) & 0xff))
+    ip.append(str((numb >> 16) & 0xff))
+    ip.append(str((numb >> 24) & 0xff))
+    ip.reverse()
+    return ".".join(ip)
